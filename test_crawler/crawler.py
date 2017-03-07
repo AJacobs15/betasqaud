81c11771bfecdb6fc6a47336e53fac9eed3dc1e5
Julian <amr@cs.uchicago.edu>
2017-03-07 17:14:42 -0600

    working:

--- test_crawler/crawler.py
+++ test_crawler/crawler.py
@@ -5,10 +5,14 @@ import queue
 import json
 import sys
 import csv
+import scraping
 
 limiting_domain = "basketball.realgm.com"
 starting_url = "http://basketball.realgm.com/nba/teams"
 limiting_path = "/nba/teams"
+
+
+
 
 
 def generate_links(initial_url):
@@ -27,10 +31,10 @@ def generate_links(initial_url):
         return [] #can't generate request
 
     proper_url = util.get_request_url(req1)
-    print(proper_url)
+    #print(proper_url)
     
     if util.is_url_ok_to_follow(proper_url, limiting_domain):
-        print("following")
+        #print("following")
         text = util.read_request(req1)
         soup = bs4.BeautifulSoup(text, "html5lib")
         links_list = soup.find_all("a", string = "Stats")
@@ -59,6 +63,114 @@ def generate_links(initial_url):
         print("else")
         return []
 
+
+    
+
+def list_to_queue(list_, q=queue.Queue()):
+    '''
+    Very simple helper function used at the beginning of crawl 
+    to put all of the starting links into the initial queue.
+    '''
+    for term in list_:
+        q.put(term)
+    return q
+
+def get_next_link(set_, queue):
+    '''
+    Check if the link has already been in the queue.
+    input: set_ is a set consiting of visited links.
+    queue is a queue consiting of links to visit.
+
+    returns: queue is the updated queue, next_in_queue is the
+    next link(if it exists), and a boolean indicating whether or not
+    a next link can be returned.
+    '''
+    if queue.empty():
+            return queue, None, False
+    else:
+            
+        next_in_queue = queue.get()
+        while next_in_queue in set_:
+            if not queue.empty():
+                next_in_queue = queue.get()
+            else:
+                return queue, None, False
+    return queue, next_in_queue, True
+
+
+
+def make_soup(initial_url, limiting_domain):
+    '''
+    Given a url and a limiting domain, returns the proper url 
+    and the BeautifulSoup object associated with that page.
+    We have this as its own function because we need to both
+    generate the links and build the index with the html page.
+    Additionally, the html page cannot be reached,
+    we return None and an empty list.
+    '''
+    req1 = util.get_request(initial_url)
+    if req1 == None:
+        return None, [] #can't generate request
+
+    proper_url = util.get_request_url(req1)
+    
+    if util.is_url_ok_to_follow(proper_url, limiting_domain):
+        text = util.read_request(req1)
+        soup = bs4.BeautifulSoup(text, "html5lib")
+        return proper_url, soup
+    else:
+        return None, []
+
+
+
+def crawl(num_pages_to_crawl,starting_url, limiting_domain):
+    '''
+    Crawls the course pages and builds the index while it goes.
+    Returns the index.
+    '''
+
+    steps = 0
+    
+    proper_url, soup = make_soup(starting_url, limiting_domain)
+    starting_links = generate_links(soup, proper_url, limiting_domain)
+    
+    return_dict = {}
+
+    visited =  {starting_url} 
+    q = list_to_queue(starting_links) 
+  
+    new_queue, next_link, indicator = get_next_link(visited, q)
+    q = new_queue
+
+    
+    while(steps <= num_pages_to_crawl and indicator):
+        visited.add(next_link) 
+        new_proper_url, rv = make_soup(next_link, limiting_domain)
+        if rv != []:
+            links = generate_links(rv, new_proper_url, limiting_domain) 
+            
+
+
+            return_dict = scraping.get_data(rv, return_dict)
+            
+            
+            for link in links:
+                if link not in visited:
+                    q.put(link) 
+
+        steps += 1 
+        new_queue, next_link, indicator = get_next_link(visited, q) 
+        q = new_queue
+
+    return return_dict
+
+
+
+
+
+
+
+'''
 def crawl(num_pages_to_crawl):
     '''
     Crawls the course page. Right now, returns a list of viable links.
@@ -93,36 +205,4 @@ def crawl(num_pages_to_crawl):
         #print("RETURN VALUE", rv)
         print(next_link)
         #print(starting_links)
-    return visited_links
-    
-
-def list_to_queue(list_, q=queue.Queue()):
-    '''
-    Very simple helper function used at the beginning of crawl 
-    to put all of the starting links into the initial queue.
-    '''
-    for term in list_:
-        q.put(term)
-    return q
-
-def get_next_link(set_, queue):
-    '''
-    Check if the link has already been in the queue.
-    input: set_ is a set consiting of visited links.
-    queue is a queue consiting of links to visit.
-
-    returns: queue is the updated queue, next_in_queue is the
-    next link(if it exists), and a boolean indicating whether or not
-    a next link can be returned.
-    '''
-    if queue.empty():
-            return queue, None, False
-    else:
-            
-        next_in_queue = queue.get()
-        while next_in_queue in set_:
-            if not queue.empty():
-                next_in_queue = queue.get()
-            else:
-                return queue, None, False
-    return queue, next_in_queue, True
+    return visited_links'''
